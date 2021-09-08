f4ba7c719e0755526ae1559182f84c77d931da49
NavSay8794 <sayyednaveed107@gmail.com>
2021-09-08 13:32:48 +0530

    Contact App Front End deployment

---
+++ src/components/ConfirmDelete.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ConfirmDelete = (props)=>{
+    console.log(props)
+    return <div> Hello Delete</div>
+    //onClick={() => { props.clickHandler(id) }}
+}
+
+export default ConfirmDelete
